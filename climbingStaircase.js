3c78295249460ef7275e0ae801ef2b3754de78ea
ridurahman <lindaridu@gmail.com>
2024-03-11 22:12:59 +0600

    Updated

--- climbingStaircase.js
+++ climbingStaircase.js
@@ -1,12 +1,13 @@
 function waysToClimbStaircase(n){
     let fNMinus1 = 1;
     let fNMinus2 = 1; 
+    let fN;
     for(let i=2; i<=n; i++){
-        let fN = fNMinus1 + fNMinus2;
+        fN = fNMinus1 + fNMinus2;
         fNMinus2 = fNMinus1;
         fNMinus1 = fN;
     }
-    return fNMinus1;
+    return fN;
 }
 let n1 = 2;
 let n2 = 3;
--- maximumProfit.js
+++ maximumProfit.js
@@ -1,9 +1,4 @@
 function buyimumProfit(prices) {
-  let buyPrice = prices[0];
-  let buyIndex = 0;
-  let sellIndex = 0;
-  let sellPrice = prices[0];
-
   let profit = 0;
   for (let i = 0; i < prices.length - 1; i++) {
     if (prices[i + 1] > prices[i]) {
@@ -14,9 +9,9 @@ function buyimumProfit(prices) {
   return profit;
 }
 
-let prices1 = [7,1,5,3,6,4];
-let prices2 = [1,2,3,4,5];
-let prices3 = [7,6,4,3,1];
+let prices1 = [7, 1, 5, 3, 6, 4];
+let prices2 = [1, 2, 3, 4, 5];
+let prices3 = [7, 6, 4, 3, 1];
 
 console.log(buyimumProfit(prices1));
 console.log(buyimumProfit(prices2));
